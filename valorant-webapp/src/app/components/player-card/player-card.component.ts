39192542b209cca0c9b562ce7d731c00926b4915
SAINTLO <cesar@macbook-pro-de-saintlo.home>
2022-06-25 19:18:10 +0200

    Player Input Added

--- valorant-webapp/src/app/components/player-card/player-card.component.ts
+++ valorant-webapp/src/app/components/player-card/player-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'player-card',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerCardComponent implements OnInit {
 
-  constructor() { }
+  @Input() PlayerRegion: string = 'United Kingdom';
+  @Input() PlayerName: string = 'phoenix';
+  PlayerImg: string =  "assets/player/player-" + this.PlayerName + ".png";
+  constructor() { 
+   
+  }
+ 
 
   ngOnInit(): void {
+    this.PlayerImg = "assets/player/player-" + this.PlayerName + ".png";
   }
 
 }
--- valorant-webapp/src/app/page/agents-page/agents-page.component.ts
+++ valorant-webapp/src/app/page/agents-page/agents-page.component.ts
@@ -7,6 +7,12 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class AgentsPageComponent implements OnInit {
 
+   players: any[] = [
+    {"name": 'phoenix', "region": "United Kingdom"},
+    {"name": 'jett',    "region": "South Korea"},
+    {"name": 'sova',    "region": "Russia"},
+    {"name": 'sage',    "region": "China"}
+  ]
 
   constructor() { }
 
